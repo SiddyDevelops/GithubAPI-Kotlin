ec04770b6ee4162bebab443bc4daf16010400f46
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-03-10 08:32:25 +0530

    Added Sus func and Coroutine scope for APIData retrival

--- app/src/main/java/com/siddydevelops/githubapi_kotlin/ApiInterfaces/ApiInterface.kt
+++ app/src/main/java/com/siddydevelops/githubapi_kotlin/ApiInterfaces/ApiInterface.kt
@@ -2,6 +2,7 @@ package com.siddydevelops.githubapi_kotlin.ApiInterfaces
 
 
 import com.siddydevelops.githubapi_kotlin.ApiModel.GithubDetailModel
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
@@ -10,6 +11,9 @@ interface ApiInterface {
 
     @GET("repos")
     fun getGithubData(): retrofit2.Call<List<GithubDetailModel>>
+    // SUSPEND function for COROUTINES
+    @GET("repos")
+    suspend fun getCoroutineGithubData(): Response<List<GithubDetailModel>>
 
     companion object {
         const val BASE_URL = "https://api.github.com/orgs/octokit/"
--- app/src/main/java/com/siddydevelops/githubapi_kotlin/MainActivity.kt
+++ app/src/main/java/com/siddydevelops/githubapi_kotlin/MainActivity.kt
@@ -5,16 +5,18 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.siddydevelops.githubapi_kotlin.ApiInterfaces.ApiInterface
 import com.siddydevelops.githubapi_kotlin.ApiModel.GithubDetailModel
 import com.siddydevelops.githubapi_kotlin.RV.RVAdapter
+import kotlinx.coroutines.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
     private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +28,8 @@ class MainActivity : AppCompatActivity() {
         val recyclerView: RecyclerView = findViewById(R.id.recyclerViewMain)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        val apiInterface = ApiInterface.create().getGithubData()
+        // Fetch data from API using conventional Retrofit-Enqueue Method
+        /* val apiInterface = ApiInterface.create().getGithubData()
         apiInterface.enqueue(object: Callback<List<GithubDetailModel>>{
             override fun onResponse(call: Call<List<GithubDetailModel>>,response: Response<List<GithubDetailModel>>) {
                 if(response.body() != null) {
@@ -43,8 +46,25 @@ class MainActivity : AppCompatActivity() {
             override fun onFailure(call: Call<List<GithubDetailModel>>, t: Throwable) {
                 Log.d(TAG, "ERROR: ${t.message}")
             }
-        })
+        }) */
 
-
+        // Fetching data from API using COROUTINES
+        launch(Dispatchers.Main) {
+            try {
+                val response = ApiInterface.create().getCoroutineGithubData()
+                if(response.isSuccessful && response.body() != null) {
+                    val apiList = response.body()!!
+                    val adapter = RVAdapter(apiList)
+                    recyclerView.adapter = adapter
+                    progressBar.visibility = View.INVISIBLE
+                } else {
+                    Log.d(TAG,"ERROR: ${response.message()}")
+                    Toast.makeText(this@MainActivity,"Error Occurred: ${response.message()}",Toast.LENGTH_SHORT).show()
+                }
+            } catch(e: Exception) {
+                Toast.makeText(this@MainActivity,"Error Occurred: ${e.message}",Toast.LENGTH_SHORT).show()
+                Log.d(TAG,"ERROR: ${e.message}")
+            }
+        }
     }
 }
